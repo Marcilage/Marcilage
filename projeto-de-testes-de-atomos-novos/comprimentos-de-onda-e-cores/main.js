44b3db1bd28a783d703ccbd3cfc51782fffa022d
Marcilage <56591412+Marcilage@users.noreply.github.com>
2021-07-15 13:34:47 -0300

    update main.js

---
+++ projeto-de-testes-de-atomos-novos/comprimentos-de-onda-e-cores/main.js
@@ -0,0 +1,20 @@
+// main.js
+console.log(`pé, iniciando dados coloridos...`);
+// alert ("test");
+ 	function modemuve() {
+			var element = document.body;
+				element.classList.toggle("dark-mode");
+}
+	function modemuve1() {
+			var element = document.body;
+					element.classList.toggle("Blue-mode");
+}
+	function modemuve2() {
+			var element = document.body;
+					element.classList.toggle("purple-mode");
+}
+	/*function modemuve3() {
+			var element = document.body;
+					element.classList.toggle("full-white");
+					 alert("Muito Branco!");
+} */
